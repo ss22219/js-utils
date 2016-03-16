867bcc94e18d1fbe4027d626d5b2d31769e1e4b2
gool <ss22219@users.noreply.github.com>
2016-03-16 10:59:19 +0800

    Update validate.js

--- jquery.fn/validate.js
+++ jquery.fn/validate.js
@@ -1,46 +1,141 @@
-﻿/*
+/*
 $('form').validate(justTest,scrollTo) -> 
-
 param: justTest bool 只是检查，不修改Dom
 param: scrollTo bool 滚动到错误元素
-
 return { isValidate : bool,
          messages:[{
                       element:jQueryEl,
                       message:string
                       }]
       }
-
 HTML:
  <form>
     Enter:
     <input type="password" data-rule="required;number;equals(target)"/>
     <input type="password" id="target"/>
  </form>
- 2015.5.19 gool */
+ 2015.3.16 gool */
 "use strict";
-
-$.fn.validate = function (justTest, scrollTo) {
-    if (this == window) {
-        return $('[data-rule]').validate(justTest, scrollTo);
-    } else if (!this.attr('data-rule') && $('[data-rule]', this).length > 0) {
-        return $('[data-rule]', this).validate(justTest, scrollTo);
-    } else if (this.validate && this.length == 0) {
-        return $('[data-rule]').validate(justTest, scrollTo);
+(function () {
+    //简单的检查是否通过验证,返回true/false
+    $.fn.isValidate = function () {
+        return this.validate.call(this, true, false).isValidate;
     }
-    var rtv = { isValidate: true, messages: [] };
-    var getArg = function (name, rule) {
+
+    //justTest 是否只是测试,默认 是
+    //scrollTo 是否滚动到错误处,默认 是
+    //validateHandler 错误处理函数, justTest=true 有默认值
+    $.fn.validate = function (justTest, scrollTo, validateHandler) {
+        if (this == window) {
+            //为所有验证控件验证
+            return $('[data-rule]').validate(justTest, scrollTo, validateHandler);
+        } else if (!this.attr('data-rule') && $('[data-rule]', this).length > 0) {
+            //如果当前元素是验证控件,为当前验证控件验证
+            return $('[data-rule]', this).validate(justTest, scrollTo, validateHandler);
+        } else if (this.validate && this.length == 0) {
+            //如果当前元素下面包含验证控件,验证当前元素下所有控件
+            return $('[data-rule]').validate(justTest, scrollTo, validateHandler);
+        }
+
+        //返回信息
+        var rtv = { isValidate: true, messages: [] };
+
+        //处理验证控件
+        $(this).each(function () {
+            var $this = $(this);
+            //获取到控件值,$this.val()需要其他组件
+            var val = $this.value || $this.val(),
+                rule = $this.attr('data-rule'), //data-rule内容
+                rs = getRules(rule); //解析成rule列表
+            for (var k in rs) {
+                var i = rs[k];
+                //对当前规则判断,如果包含正则表达式属性,使用正则验证,否则使用自定义action验证
+                if (rules[i] &&
+                    (rules[i].rule ? !rules[i].rule.test(val) : true) //正则验证
+                    &&
+                    (rules[i].action ? rules[i].action($this, rule, val) : true) //自定义action验证
+                    ) {
+                    rtv.isValidate = false;
+                    rtv.messages.push({ element: $this, message: rules[i].message }); //设置验证失败信息
+                }
+            }
+        });
+
+
+        //默认的验证结果处理
+        if (justTest !== true && !validateHandler)
+            validateHandler = function (msg) {
+                if (msg.isValidate)
+                    $(this).parent().find('.error_msg').remove();
+                else
+                    $(msg.messages).each(function () {
+                        var el = this.element;
+                        $('.error_msg', el.parent()).remove();
+                        el.after('<div class="error_msg">' + this.message + '</div>');
+                        if (scrollTo !== false)
+                            $(window).scrollTop(el.offset().top);
+                    });
+            }
+
+        if (validateHandler)
+            validateHandler.call(this, rtv);
+        return rtv;
+    }
+
+    //getArg('number;length(18)','length') -> 18 现在只支持一个参数
+    function getArg(name, rule) {
         var reg = new RegExp(name + '\\s?\\(\\s?(.+?)\\s?\\)');
         return reg.exec(rule)[1];
     }
+
+    //解析data-rule信息
+    function getRules(rule) {
+        if (!rule)
+            return [];
+        var rs = rule.split(';'), len = rs.length, s = [], reg = /\w+/, tmp;
+        for (var i = 0; i < len ; i++) {
+            if (rs[i]) {
+                var tmp = reg.exec(rs[i]);
+                if (tmp)
+                    s.push(tmp[0].trim());
+            }
+        }
+        return s;
+    }
+
+    //验证规则 number,required,datetime,phone是正则规则 其他的是自定义action验证
     var rules = {
-        number: { rule: /^\d{0,}$/, message: '必须是一个整数' },
+        number : { rule: /^\d{0,}$/, message: '必须是一个整数' },
 
-        required: { rule: /.+/, message: '不能为空' },
+        required : { rule: /.+/, message: '不能为空' },
 
-        datetime: { rule: /^(\d{4}\/\d{1,2}\/\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2})?$/, message: '不是有效的日期格式' },
+        datetime : { rule: /^(\d{4}\/\d{1,2}\/\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2})?$/, message: '不是有效的日期格式' },
+
+        phone: { rule: /^(13[0-9]|14[0-9]|15[0-9]|18[0-9])\d{8}$/i, message: '不是有效的手机号码' },
+
+        length: {
+            last: 0,//保存起来的data-rule中的参数值
+            action: function (el, rule, val) {
+                if (val === null || typeof val == 'undefined' || val === '')
+                    return;
+                try {
+                    var length = el.data("length");
+                    if (!length) {
+                        length = getArg('length', rule); //获取规则参数 length(18) -> 18
+                        el.data("length", length);
+                    }
+                    rules.length.last = length;
+                    return val.length != length;
+                } catch (e) {
+                    console.error('length rule error!');
+                }
+            }, get message() {
+                return '长度必须为' + rules.length.last;
+            }
+        },
+
         min: {
-            last: 0,
+            last: 0,//保存起来的data-rule中的参数值
             action: function (el, rule, val) {
                 if (val === null || typeof val == 'undefined' || val === '')
                     return;
@@ -60,7 +155,7 @@ $.fn.validate = function (justTest, scrollTo) {
             }
         },
         max: {
-            last: 0,
+            last: 0,//保存起来的data-rule中的参数值
             action: function (el, rule, val) {
                 if (val === null || typeof val == 'undefined' || val === '')
                     return;
@@ -82,14 +177,14 @@ $.fn.validate = function (justTest, scrollTo) {
         equals: {
             action: function (el, rule, val) {
                 try {
-                    target = getArg('equals', rule);
+                    var target = getArg('equals', rule);
                     return val !== $('#' + target).val();
                 } catch (e) {
                     console.error('validate equals rule error!');
                 }
             }, message: '两次输入的值不一致'
         }, maxLength: {
-            lastLength: 0,
+            lastLength: 0,//保存起来的data-rule中的参数值
             action: function (el, rule, val) {
                 try {
                     var length = el.data("maxLength");
@@ -106,7 +201,7 @@ $.fn.validate = function (justTest, scrollTo) {
                 return '长度不能大于' + rules.maxLength.lastLength;
             }
         }, minLength: {
-            lastLength: 0,
+            lastLength: 0,//保存起来的data-rule中的参数值
             action: function (el, rule, val) {
                 try {
                     var length = el.data("minLength");
@@ -124,64 +219,5 @@ $.fn.validate = function (justTest, scrollTo) {
             }
         }
     };
-    function getRules(rule) {
-        if (!rule)
-            return [];
-        var rs = rule.split(';'), len = rs.length, s = [], reg = /\w+/, tmp;
-        for (var i = 0; i < len ; i++) {
-            if (rs[i]) {
-                var tmp = reg.exec(rs[i]);
-                if (tmp)
-                    s.push(tmp[0].trim());
-            }
-        }
-        return s;
-    }
-    $(this).each(function () {
-        var $this = $(this);
-        if (justTest !== true) {
-            var checker = function () {
-                $this.validate(false, false);
-            }
-            $this.bind('blur', checker);
-        }
-        var val = $this.value || $this.val(), rule = $this.attr('data-rule'), rs = getRules(rule);
-        for (var k in rs) {
-            var i = rs[k];
-            if (rules[i] && (rules[i].rule ? !rules[i].rule.test(val) : true) && (rules[i].action ? rules[i].action($this, rule, val) : true)) {
-                rtv.isValidate = false;
-                rtv.messages.push({ element: $this, message: rules[i].message });
-            }
-        }
-    });
-
-
-    if (justTest)
-        return rtv;
-    if (window.validateHandler)
-        validateHandler.call(this, rtv);
-    else {
-        var getParent = function (el) {
-            var el = el[0];
-            while (el && el.tagName && el.tagName.toLowerCase() != 'tr' && el.tagName.toLowerCase() != 'body')
-                el = $(el).parent()[0];
-            return $(el);
-        };
-        if (rtv.isValidate)
-            getParent(this).find('.error_msg').remove();
-        else
-            $(rtv.messages).each(function () {
-                var el = getParent(this.element);
-                el.find('.error_msg').remove();
-                if (/^\s+$/.test(el.find('td').last().html()))
-                    el.find('td').last().remove();
-                el.find('td').last().after('<td class="error_msg">' + this.message + '</td>');
-                if (scrollTo !== false)
-                    $(window).scrollTop(el.offset().top);
-            });
-    }
-    return rtv;
-}
-$.fn.isValidate = function (justTest, scrollTo) {
-    return this.validate.call(this, justTest, scrollTo).isValidate;
-}
+    $.fn.validate.rules = rules;
+})()
